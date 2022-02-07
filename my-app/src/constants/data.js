26b4815e33a33b47234cf585bf391d9973083c1b
Kornchangetheworld <95617311+Kornchangetheworld@users.noreply.github.com>
2022-02-07 09:20:44 +0700

    ok

---
+++ my-app/src/constants/data.js
@@ -0,0 +1,82 @@
+import images from './images';
+
+const wines = [
+  {
+    title: 'Chapel Hill Black Truffle',
+    price: '$8.8',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Catena Paprika',
+    price: '$8',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'La Barbecue',
+    price: '$8',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Rhino Cheese',
+    price: '$8',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Málà',
+    price: '$8',
+    tags: 'AU | Bottle',
+  },
+];
+
+const cocktails = [
+  {
+    title: 'Black Truffle',
+    price: '$6',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: "Catena Paprika",
+    price: '$6',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'La Barbecue',
+    price: '$6',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Rhino Cheese',
+    price: '$6',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Málà',
+    price: '$6',
+    tags: 'AU | Bottle',
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.award02,
+    title: 'Bib Gourmond',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award01,
+    title: 'Rising Star',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award05,
+    title: 'AA Hospitality',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award03,
+    title: 'Outstanding Chef',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+];
+
+export default { wines, cocktails, awards };
